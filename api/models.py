8e0e560a9940c3fc9f72e182d679a40633cdc651
Masud <ec20503@qmul.ac.uk>
2023-12-01 22:40:07 +0000

    Added user class with profile pic
    Issues with image not diplaying still to fix, configures settings.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    profile_pic = models.ImageField(null=True, blank=True)
+
+
--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL= '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -134,3 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+AUTH_USER_MODEL = 'api.User'
